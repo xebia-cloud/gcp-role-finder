049c984fbbec182130aa8824b28fce1a47a34769
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-01-27 23:28:06 +0100

    feat: purple-ish color

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,24 @@
 import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+    Admin,
+    Resource,
+    ListGuesser,
+    EditGuesser,
+    ShowGuesser, defaultTheme,
 } from "react-admin";
 import { RoleList, RoleShow } from "./Roles";
 import { dataProvider } from "./dataProvider";
+import {Dashboard} from "./Dashboard";
+import purple from '@mui/material/colors/purple';
+
+const theme = {
+    ...defaultTheme,
+    palette: {
+        primary: purple
+    },
+};
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} theme={theme}>
     <Resource
       name="roles"
       list={RoleList}
---
+++ website/src/Dashboard.tsx
@@ -0,0 +1,11 @@
+import {Card, CardContent, CardHeader} from "@mui/material";
+
+export const Dashboard = () => (
+    <Card>
+        <CardHeader title="Google Cloud Platform IAM Role finder"/>
+        <CardContent>
+            Fulltext search on the Google Cloud Platform IAM roles in your project and organization, intended
+            to help you find the least privileged role for your actors.
+        </CardContent>
+    </Card>
+);
